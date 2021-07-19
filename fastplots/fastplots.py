f2afb621e8633190a47bf399e0ec593826473887
Jhosscryte <61122678+Jhosscryte@users.noreply.github.com>
2021-07-19 16:52:46 -0500

    Update fastplots.py

--- fastplots/fastplots.py
+++ fastplots/fastplots.py
@@ -1,10 +1,32 @@
-def convert(my_name):
-    """
-    Print a line about converting a notebook.
-    Args:
-        my_name (str): person's name
-    Returns:
-        None
-    """
+import numpy as np
+import matplotlib.pyplot as plt
 
-    print(f"I'll convert a notebook for you some day, {my_name}.")
+def margen(arreglo,porcentaje):
+    minimo = min(arreglo)
+    maximo = max(arreglo)
+    porciento = porcentaje*(maximo-minimo)/100
+    return (-minimo-porciento, maximo+porciento)
+
+def grafico(a,b,linea=".",leyenda=None,xmargen=None,ymargen=None,titulo=None,xlabel=None,ylabel=None,grid=True):
+    
+    # ¿con o sin leyenda?
+    if leyenda!=None:
+        plt.plot(a,b,linea,label=leyenda)
+        plt.legend()
+    else: plt.plot(a,b,linea)
+        
+    # títulos y labels
+    plt.title(titulo)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    
+    # márgenes
+    if xmargen!=None:
+        plt.xlim(margen(a,xmargen))
+    if ymargen!=None:
+        plt.ylim(margen(b,ymargen))
+        
+    # ¿con o sin grid?
+    if grid==True: plt.grid()
+    
+    plt.show()
