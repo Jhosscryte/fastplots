9c4de92d34495bcb99a33ca6f6c274d5bd17d17f
Jhosscryte <61122678+Jhosscryte@users.noreply.github.com>
2021-07-24 18:34:29 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 #from fastplots.graficos.funciones import *
 #import fastplots.graficos.funciones as graficos
 
-import fastplots.graficos
-import fastplots.esferas
+#import fastplots.graficos
+#import fastplots.esferas
