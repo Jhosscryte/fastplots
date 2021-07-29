430270646cdf7b5d05c2eb9ac420f1d5e8aa77ab
Jhosscryte <61122678+Jhosscryte@users.noreply.github.com>
2021-07-28 23:12:08 -0500

    Create codigo.py

---
+++ constantes/codigo.py
@@ -0,0 +1,14 @@
+"""Este módulo contiene los valores de las constantes universales en el
+Sistema Internacional (SI) con todas las cifras significativas conocidas"""
+
+from numpy import pi
+G = 6.67430e-11         #Constante de gravitación universal
+h = 6.62607015e-34      #Constante de Plank
+c = 299792458           #Velocidad de la luz
+e0 = 8.8541878176e-12   #Permitividad eléctrica en el vacío
+mu0 = 4*pi*1e-7         #Permeabilidad magnética del vacío
+e = 1.602176634e-19     #Carga del electrón
+N = 6.02214076e23       #Número de Avogadro
+k = 1.380649e-23        #Constante de Boltzmann
+me = 9.1093837015e-31   #Masa del electrón
+mp = 1.67262192369e-27  #Masa del protón
